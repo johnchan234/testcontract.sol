f9d0161298f7c99abf14a7ceb32da53b897f535f
him1019 <hoihim1019@gmail.com>
2021-06-14 17:23:35 +0800

    fist init

--- Temp.sol
+++ Temp.sol
@@ -1,13 +1,7 @@
 pragma solidity ^0.5.11;
 
-library  Temp {
-    
-    string public name= "im temp contrat";
-    uint public number ;
-    
-    function setName(uint _num) public {
-        number = _num;
-    }
+library   Temp {
+  
     function returnNumber(uint _num) public pure returns (uint){
         return _num+1;
     }
